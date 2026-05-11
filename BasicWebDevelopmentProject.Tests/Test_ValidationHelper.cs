5c65c107f16c133d3d97cffa689e5a172d9b2162
PKPakpoom <pai38985@gmail.com>
2026-05-11 17:51:58 +0700

    fix: some testcase does't have TestAttribute

--- BasicWebDevelopmentProject.Tests/Test_ValidationHelper.cs
+++ BasicWebDevelopmentProject.Tests/Test_ValidationHelper.cs
@@ -15,31 +15,31 @@ namespace BasicWebDevelopmentProject.Tests
             string card_id = "4549420001927672";
             Assert.That(ValidationHelper.ValidateCreditCard(card_id), Is.EqualTo(true));
         }
-
+        [Test]
         public void test_card_validation_2()
         {
             string card_id = "4549420001927671";
             Assert.That(ValidationHelper.ValidateCreditCard(card_id), Is.EqualTo(false));
         }
-
+        [Test]
         public void test_card_validation_3()
         {
             string card_id = "532352";
             Assert.That(ValidationHelper.ValidateCreditCard(card_id), Is.EqualTo(false));
         }
-
+        [Test]
         public void test_id_validation_1()
         {
             string card_id = "8121389985207";
             Assert.That(ValidationHelper.ValidateCitizenId(card_id), Is.EqualTo(true));
         }
-
+        [Test]
         public void test_id_validation_2()
         {
             string card_id = "8121389985201";
             Assert.That(ValidationHelper.ValidateCitizenId(card_id), Is.EqualTo(false));
         }
-
+        [Test]
         public void test_id_validation_3()
         {
             string card_id = "23423";
